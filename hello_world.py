af67c1f8aae4c00be16e4204736c110f35485d39
aryeshuacheng <andrew.young.cheng@gmail.com>
2020-05-16 22:05:46 -0400

    Hmm... it's a start

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,7 @@
-from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication
-import sys, random
+import sys
+from PyQt5.QtWidgets import (QMainWindow, QFrame, QWidget, QGridLayout, QPushButton, QApplication, QDesktopWidget)
 
-class Chess(QMainWindow):
+class Chess(QWidget):
 
     def __init__(self):
         super().__init__()
@@ -12,13 +12,24 @@ class Chess(QMainWindow):
         # Initiates application UI
 
         self.chess_board = Board(self)
-        self.setCentralWidget(self.chess_board)
-
         self.chess_board.start()
 
-        self.resize(900, 900)
+        chess_board_layout = QGridLayout()
+        self.setLayout(chess_board_layout)
+
+        # Setup board
+        for x in range(9):
+            for y in range(9):
+                if x == 1:
+                    chess_board_layout.addWidget(QPushButton("White Pawn"), x, y)
+                if x > 1 and x < 7:
+                    chess_board_layout.addWidget(QPushButton("Free Space"), x, y)
+                if x == 7:
+                    chess_board_layout.addWidget(QPushButton("Black Pawn"), x, y)
+
+        self.resize(600, 600)
         self.center()
-        self.setWindowTitle('Tetris')        
+        self.setWindowTitle('Chess')        
         self.show()
 
     def center(self):
@@ -29,9 +40,6 @@ class Chess(QMainWindow):
             (screen.height()-size.height())/2)
 
 class Board(QFrame):
-    BoardWidth = 8
-    BoardHeight = 8
-
     def __init__(self, parent):
         super().__init__(parent)
         
@@ -46,16 +54,9 @@ class Board(QFrame):
 
         self.isStarted = True
 
-        # I think we setup the initial configuration of the board here
-
-        # Add White pieces here
-        # {WhiteKingSideBishop: {position: [0,0]} i.e. poisiton: [x,y] on a 8x8 grid
-
-        # Add Black pieces here
-
 # Starts the application
 if __name__ == '__main__':
     
     app = QApplication([])
-    tetris = Chess()    
+    chess = Chess()    
     sys.exit(app.exec_())
